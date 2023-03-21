5e48c77b4ab2f8a70c098a6e96f16747413a8ee2
Tobias Zimmermann <tobias.zimmer007@gmail.com>
2023-03-21 22:28:53 +0100

    feat: start merge sort

--- lib-rust/src/lib.rs
+++ lib-rust/src/lib.rs
@@ -3,7 +3,8 @@
 pub mod sort;
 
 use jni::{
-    objects::{JClass, JIntArray},
+    errors::Result,
+    objects::{JClass, JIntArray, JPrimitiveArray},
     JNIEnv,
 };
 
@@ -16,6 +17,19 @@ pub extern "system" fn Java_com_groupawesome_example_rustlib_Sort_bubbleSort<'lo
     unsafe {
         if let Ok(mut arr) = env.get_array_elements(&arr, jni::objects::ReleaseMode::CopyBack) {
             sort::bubble_sort(&mut arr);
+        }
+    }
+}
+
+#[no_mangle]
+pub extern "system" fn Java_com_groupawesome_example_rustlib_Sort_mergeSort<'local>(
+    mut env: JNIEnv<'local>,
+    _jclass: JClass,
+    arr: JIntArray<'local>,
+) {
+    unsafe {
+        if let Ok(mut arr) = env.get_array_elements(&arr, jni::objects::ReleaseMode::CopyBack) {
+            sort::merge_sort(&mut arr);
         }
     }
 }
--- lib-rust/src/sort.rs
+++ lib-rust/src/sort.rs
@@ -2,8 +2,8 @@ use jni::objects::AutoElements;
 
 // TODO: Ensure that we are doing bubble sort the most optimally
 pub fn bubble_sort(arr: &mut AutoElements<i32>) {
-    for _i in 0..arr.len() {
-        for j in 0..arr.len() - 1 {
+    for i in 0..arr.len() {
+        for j in 0..arr.len() - 1 - i {
             if arr[j] > arr[j + 1] {
                 let curr = arr[j];
                 let prev = arr[j + 1];
@@ -14,13 +14,40 @@ pub fn bubble_sort(arr: &mut AutoElements<i32>) {
     }
 }
 
-fn merge_sort(arr: &mut AutoElements<i32>) {
-    todo!()
+pub fn merge_sort(arr: &mut AutoElements<i32>) {
+    merge_sort::sort(arr, 0, (arr.len() - 1) as isize)
+}
+
+mod merge_sort {
+    use jni::objects::AutoElements;
+
+    pub fn sort(arr: &mut AutoElements<i32>, left: isize, right: isize) {
+        if left > right {
+            return;
+        }
+        let middle = left + (right - left) / 2;
+        sort(arr, left, middle - 1);
+        sort(arr, middle + 1, right);
+        merge(arr, left as usize, middle as usize, right as usize)
+    }
+
+    fn merge(arr: &mut AutoElements<i32>, l: usize, m: usize, r: usize) {
+        let mut left: Vec<i32> = vec![0; m - l + 1];
+        let mut right: Vec<i32> = vec![0; r - m];
+        for i in 0..(m - l + 1) {
+            left[i] = arr[l + i];
+        }
+        for i in 0..(r - m) {
+            right[i] = arr[m + 1 + i];
+        }
+        // Merge the vectors where the smaller ones goes in front
+        println!("left: {:?}, right: {:?}", left, right);
+    }
 }
 
 #[cfg(test)]
 mod test {
-    use jni::{InitArgsBuilder, JNIVersion, JavaVM};
+    use jni::{objects::ReleaseMode, InitArgsBuilder, JNIVersion, JavaVM};
     use lazy_static::lazy_static;
 
     use super::*;
@@ -37,25 +64,19 @@ mod test {
 
     #[test]
     fn can_bubble_sort() {
-        test_sort(|arr| {
-            bubble_sort(arr);
-        })
+        test_sort(bubble_sort)
     }
 
     #[test]
     fn can_merge_sort() {
-        test_sort(|arr| {
-            merge_sort(arr);
-        })
+        test_sort(merge_sort)
     }
 
     fn test_sort(sort: fn(&mut AutoElements<i32>)) {
-        let mut env = VM.attach_current_thread().unwrap();
+        let mut env = VM.attach_current_thread_as_daemon().unwrap();
         if let Ok(arr) = env.new_int_array(5) {
             unsafe {
-                if let Ok(mut arr) =
-                    env.get_array_elements(&arr, jni::objects::ReleaseMode::CopyBack)
-                {
+                if let Ok(mut arr) = env.get_array_elements(&arr, ReleaseMode::CopyBack) {
                     arr[0] = 5;
                     arr[1] = 2;
                     arr[2] = 10;
@@ -63,11 +84,7 @@ mod test {
                     arr[4] = -2;
                     sort(&mut arr);
                     assert!(
-                        arr[0] == -2 &&
-                        arr[1] == 0 &&
-                        arr[2] == 2 &&
-                        arr[3] == 5 &&
-                        arr[4] == 10
+                        arr[0] == -2 && arr[1] == 0 && arr[2] == 2 && arr[3] == 5 && arr[4] == 10
                     );
                 }
             }
