7457d21528209aea3f7432faf87d2a624a429746
Mathias Jensen <mathias@Mathiass-MacBook-Pro.local>
2023-03-24 10:15:39 +0100

    Spring Boot App

---
+++ ufoproject/src/main/java/com/example/ufoproject/UfoprojectApplication.java
@@ -0,0 +1,13 @@
+package com.example.ufoproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UfoprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UfoprojectApplication.class, args);
+	}
+
+}
---
+++ ufoproject/src/test/java/com/example/ufoproject/UfoprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ufoproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UfoprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
