a1888b11895db8443c008cbeee3a2290fdf16477
Tobias Zimmermann <tobias.zimmer007@gmail.com>
2023-03-24 15:57:05 +0100

    feat: docker setup

--- ufoproject/src/main/java/com/example/ufoproject/rust/RustConfig.java
+++ ufoproject/src/main/java/com/example/ufoproject/rust/RustConfig.java
@@ -1,11 +1,22 @@
 package com.example.ufoproject.rust;
 
+import jakarta.annotation.PostConstruct;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@Slf4j
 public class RustConfig {
-    static {
-        // Load the implementations from our rust library
-        System.loadLibrary("lib_rust");
+
+    @PostConstruct
+    private void postConstruct() {
+        try {
+            // Load the implementations from our rust library
+            System.loadLibrary("lib_rust");
+        } catch (Throwable e) {
+            log.error("unable to load the native rust library. Java could not find it in the paths that your OS requires it", e);
+            log.info("to make it work follow the guide written in the Native Integration section of the README.md");
+        }
     }
+
 }
