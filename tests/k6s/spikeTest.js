1d9949337a0c7a5c8d91443e0b3ad7634323a528
Tobias Zimmermann <tobias.zimmer007@gmail.com>
2023-04-09 11:51:32 +0200

    fix: change file paths and upp the target for spike tests

--- tests/k6s/spikeTest.js
+++ tests/k6s/spikeTest.js
@@ -11,8 +11,8 @@ export let options = {
     stages: [
         { duration: "10s", target: 100 }, // below normal load
         { duration: "1m", target: 100 },
-        { duration: "10s", target: 300 }, // spike to 300 users
-        { duration: "3m", target: 300 }, // stay at 300 for 3 minutes
+        { duration: "10s", target: 500 }, // spike to 500 users
+        { duration: "3m", target: 500 }, // stay at 500 for 3 minutes
         { duration: "10s", target: 100 }, // scale back down
         { duration: "3m", target: 100 },
         { duration: "10s", target: 0 },
@@ -20,7 +20,7 @@ export let options = {
 }
 
 const body = new SharedArray('some name', function() {
-    return parse(open(`/${__ENV.HOME}/tests/s/${__ENV.TEST_AMOUNT}.txt`));
+    return parse(open(`/${__ENV.HOME}/tests/documents/unsorted/${__ENV.TEST_AMOUNT}.txt`));
 });
 
 function parse(text) {
